4425114860f3349f25e7f1546ab2c5921eef5878
william-carvalho <will.carbas@gmail.com>
2025-10-23 11:37:37 -0300

    Verbos HTTP POST, PUT e DELETE

--- rest-spring-boot-java-will/src/main/java/br/com/will/controllers/PersonController.java
+++ rest-spring-boot-java-will/src/main/java/br/com/will/controllers/PersonController.java
@@ -4,10 +4,9 @@ import br.com.will.model.Person;
 import br.com.will.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -17,8 +16,30 @@ public class PersonController {
     private PersonServices personServices;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable("id") String id){
+    public Person findById(@PathVariable("id") String id) {
         return personServices.findById(id);
 
     }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return personServices.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return personServices.create(person);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return personServices.update(person);
+    }
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id) {
+        personServices.delete(id);
+    }
+
 }
--- rest-spring-boot-java-will/src/main/java/br/com/will/services/PersonServices.java
+++ rest-spring-boot-java-will/src/main/java/br/com/will/services/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.will.services;
 import br.com.will.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,7 +15,7 @@ public class PersonServices {
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
-    public Person findById(String id){
+    public Person findById(String id) {
 
         logger.info("Find one person");
 
@@ -27,4 +29,47 @@ public class PersonServices {
         return person;
 
     }
+
+    public Person create(Person person) {
+
+        logger.info("Creating one Person!");
+
+        return person;
+    }
+
+    public Person update(Person person) {
+
+        logger.info("Updating one Person!");
+
+        return person;
+    }
+
+    public void delete(String id) {
+
+        logger.info("Deleting one Person!");
+
+    }
+
+    public List<Person> findAll() {
+
+        logger.info("Finding all People!");
+
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Firstname " + i);
+        person.setLastName("Lastname " + i);
+        person.setAddress("Some Address in Brasil");
+        person.setGender("Male");
+        return person;
+    }
+
 }
