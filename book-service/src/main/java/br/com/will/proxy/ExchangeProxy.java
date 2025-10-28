d56423c47d8ffc91539dc0e1f267892717044449
william-carvalho <will.carbas@gmail.com>
2025-10-28 18:51:53 -0300

    Balanceamento de Carga entre Microsserviços com Spring Cloud API Gateway

--- book-service/src/main/java/br/com/will/proxy/ExchangeProxy.java
+++ book-service/src/main/java/br/com/will/proxy/ExchangeProxy.java
@@ -2,13 +2,12 @@ package br.com.will.proxy;
 
 import br.com.will.dto.ExchangeDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.math.BigDecimal;
 
-@FeignClient(name = "exchange-service", url = "localhost:8000" )
+@FeignClient(name = "exchange-service")
 public interface ExchangeProxy {
 
     @GetMapping(value = "/exchange-service/{amount}/{from}/{to}")
