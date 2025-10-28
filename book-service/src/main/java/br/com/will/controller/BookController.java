71f9adbb68fc5c3cdaf836030cbe39895da84471
william-carvalho <will.carbas@gmail.com>
2025-10-28 20:52:40 -0300

    Configurando o Swagger OpenAPI no Book Microservice

--- book-service/src/main/java/br/com/will/controller/BookController.java
+++ book-service/src/main/java/br/com/will/controller/BookController.java
@@ -5,6 +5,8 @@ import br.com.will.enverionment.InstanceInformationService;
 import br.com.will.model.Book;
 import br.com.will.proxy.ExchangeProxy;
 import br.com.will.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Date;
 import java.util.HashMap;
 
+@Tag(name = "Book Endpoint")
 @RestController
 @RequestMapping("book-service")
 public class BookController {
@@ -42,6 +45,7 @@ public class BookController {
         return book;
     }
 
+    @Operation(summary = "Find a specific book by your ID")
     @GetMapping(value ="/{id}/{currency}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency){
 
--- book-service/src/main/java/br/com/will/controller/FooBarController.java
+++ book-service/src/main/java/br/com/will/controller/FooBarController.java
@@ -8,6 +8,7 @@ import br.com.will.repository.BookRepository;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Tag(name = "Foo Bar Endpoint")
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
